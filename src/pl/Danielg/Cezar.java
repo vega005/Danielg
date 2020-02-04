0c2ef6c0c116750424f6f63997e563f774249168
vega005 <43038140+vega005@users.noreply.github.com>
2020-02-04 12:46:03 +0100

    Update Cezar.java

--- src/pl/Danielg/Cezar.java
+++ src/pl/Danielg/Cezar.java
@@ -2,22 +2,21 @@ package pl.Danielg;
 
 public class Cezar {
   public static void main(String[] args) {
-    String szyfrowanie = code(pl.Danielg.Rekurencja.poprosOhaslo(), 3);//przypisanie do zmiennej metody code w ktorej wywoluje sie klase rekurencja, wpisuaje haslo
-    System.out.println(szyfrowanie);//koduje i wyswietla ////stworzenie zmiennej o ile ma sie przeniesc kodowany znak
+    String szyfrowanie = code(pl.Danielg.Rekurencja.poprosOhaslo(), 3); 
+    System.out.println(szyfrowanie);
   }
 
-  public static String code(String message, int przeniesienie) {//stworzenie metody code z argumentem message, ktora za zadanie kodowac podana wiadomosc
-    StringBuilder stringBuilder = new StringBuilder(message);//stworzenie StringB ktory ma dzialac na podanej wiadomosci
-    for (int i = 0; i < stringBuilder.length(); i++) {//stworzenie petli ktora bedzie sie powtarzac do o 1 mniej dlugosci SB (wiadomosci)
-      int c = stringBuilder.charAt(i);//zmienna ktora znaleziony znak w SB otrzyma jego pozycje na tablicy unicode
-      if (c + przeniesienie > 127) {//jezeli pozycja znaku + przeniesienie jest mniejsze od pozycji znaku na 127
-        c = 46 + (przeniesienie - 127 + c);//wtedy do pozycji znaku przypisuje pozycje o jeden mnijesza znaku od ktorego chce zaczac plus wartosc przeniesienia - max dlugosc alfabetu + pozycja c
+  public static String code(String message, int przeniesienie) {
+    StringBuilder stringBuilder = new StringBuilder(message);
+    for (int i = 0; i < stringBuilder.length(); i++) {
+      int c = stringBuilder.charAt(i);
+      if (c + przeniesienie > 127) {
+        c = 46 + (przeniesienie - 127 + c);
       } else {
-        c += przeniesienie;//w przeciwnym razie  pozycja znaku plus przeniesienie
+        c += przeniesienie;
       }
-      stringBuilder.setCharAt(i, (char) c);//wartosc znaku c zmieniony na char
+      stringBuilder.setCharAt(i, (char) c);
     }
-    return stringBuilder.toString();//zwrot SB w stringu
+    return stringBuilder.toString();
   }
 }
-
