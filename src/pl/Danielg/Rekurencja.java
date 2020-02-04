432901cdf4c3992a09f3765b8782b5b8ca0b64d7
vega005 <43038140+vega005@users.noreply.github.com>
2020-02-04 12:42:30 +0100

    Update Rekurencja.java

--- src/pl/Danielg/Rekurencja.java
+++ src/pl/Danielg/Rekurencja.java
@@ -7,19 +7,19 @@ public class Rekurencja {
     poprosOhaslo();
   }
 
-  public static String poprosOhaslo(){ //dodac komentarze
-    Scanner scanner = new Scanner(System.in); //tworzenie obiektu scanera
-    String password; // utworzenie zmiennej haslo
-    int passwordStrength = 0; // utworzenie zmiennej liczacej sile hasla
-    System.out.println("Podaj hasło: " );// wyswietla prosbe o podanie hasla
-    password = scanner.nextLine();//przypisanie do zmiennej haslo obiektu scanera
-    passwordStrength = Haslo.checkPasswordStrength(password);//przypisanie do zmiennej silaHasla klasy Haslo z wywolaniem metody sprawwdzenia sily hasla po jego wpisaniu
-    if (passwordStrength >= 3 && password.length() > 8){//warunek sprawdzajacy czy sila hasla jest wieksza badz rowna 3 i dlugosci 8
-      System.out.println("Haslo jest wystarczajaco mocne:" + " " + passwordStrength);// wyswietlenie informacji ,ze haslo jest wystarczajaco sile
+  public static String poprosOhaslo(){ 
+    Scanner scanner = new Scanner(System.in); 
+    String password; 
+    int passwordStrength = 0; 
+    System.out.println("Podaj hasło: " );
+    password = scanner.nextLine();
+    passwordStrength = Haslo.checkPasswordStrength(password);
+    if (passwordStrength >= 3 && password.length() > 8){
+      System.out.println("Haslo jest wystarczajaco mocne:" + " " + passwordStrength);
     }else {
-      poprosOhaslo();//jesli nie jest to Rekurencyjnie uruchom ta metode ponownie
+      poprosOhaslo();
     }
-    //System.out.println(passwordStrength);//wyswietl zebrana sile hasla
+    //System.out.println(passwordStrength);
     return password;//zwroc haslo
   }
 }
